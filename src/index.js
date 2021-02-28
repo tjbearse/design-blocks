1767e8cae99156ef1652e5a789ab3018cfb555fe
TJ Bearse <bearse.thomas@gmail.com>
2021-02-28 02:20:14 -0800

    zoom

--- src/index.js
+++ src/index.js
@@ -13,7 +13,16 @@ var workspace = Blockly.inject(blocklyDiv, {
 		length: 3,
 		colour: '#ccc',
 		snap: true
-	}
+	},
+	zoom: {
+		controls: true,
+		wheel: true,
+		startScale: 1.0,
+		maxScale: 3,
+		minScale: 0.3,
+		scaleSpeed: 1.2,
+		pinch: true
+	},
 });
 
 // save / restore to local storage
